54baf0a9f0889f3c6ccce7f9d3490b75fbc1c07f
Aspasija Cvetkoska <aspasija.codes@gmail.com>
2024-03-20 09:44:48 +0100

    added appointments table and total cost for the appointments

--- Client/src/Components/DatePicker/DateRangePicker.jsx
+++ Client/src/Components/DatePicker/DateRangePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { DatePicker } from 'antd';
 import moment from 'moment';
 const { RangePicker } = DatePicker;
--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -5,6 +5,7 @@ import { FaCircle } from "react-icons/fa";
 import DateRangePicker from "./DatePicker/DateRangePicker";
 import { FaCalendarAlt } from "react-icons/fa";
 import { RiAdminFill } from "react-icons/ri";
+import moment from "moment";
 
 const Home = () => {
   const [adminTotal, setAdminTotal] = useState();
@@ -12,6 +13,8 @@ const Home = () => {
   const [petOwnersTotal, setPetOwnersTotal] = useState();
   const [petsTotal, setPetsTotal] = useState();
   const [admins, setAdmins] = useState([]);
+  const [events, setEvents] = useState([]);
+  const totalCost = events.reduce((acc, event) => acc + event.service_price, 0);
 
   useEffect(() => {
     adminCount();
@@ -48,23 +51,19 @@ const Home = () => {
   };
 
   const petsCount = () => {
-    axios
-      .get("http://localhost:3000/auth/pets-count")
-      .then((result) => {
-        if (result.data.Status) {
-          setPetsTotal(result.data.Result[0].pets);
-        }
-      });
+    axios.get("http://localhost:3000/auth/pets-count").then((result) => {
+      if (result.data.Status) {
+        setPetsTotal(result.data.Result[0].pets);
+      }
+    });
   };
 
   const petOwnersCount = () => {
-    axios
-      .get("http://localhost:3000/auth/pet-owners-count")
-      .then((result) => {
-        if (result.data.Status) {
-          setPetOwnersTotal(result.data.Result[0].petowners);
-        }
-      });
+    axios.get("http://localhost:3000/auth/pet-owners-count").then((result) => {
+      if (result.data.Status) {
+        setPetOwnersTotal(result.data.Result[0].petowners);
+      }
+    });
   };
 
   const handleDeleteAdmin = (id) => {
@@ -78,6 +77,21 @@ const Home = () => {
         }
       });
   };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auth/appointments-combined")
+      .then((result) => {
+        console.log(result.data); // Log the data received from the API
+        if (result.data.status) {
+          setEvents(result.data.result);
+        } else {
+          alert(result.data.error);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  
 
   return (
     <div>
@@ -99,7 +113,7 @@ const Home = () => {
           <hr />
           <div className="d-flex justify-content-between">
             <h5>Total:</h5>
-            <h5>{veterinariansTotal? veterinariansTotal : 0}</h5>
+            <h5>{veterinariansTotal ? veterinariansTotal : 0}</h5>
           </div>
         </div>
         <div className="px-3 mx-4 pt-2 pb-3 border shadow-sm w-25">
@@ -132,7 +146,10 @@ const Home = () => {
             <Link to="/dashboard/preview-appointment" className="btn btn-light">
               Calendar
             </Link>
-            <Link to="/dashboard/add-appointment" className="btn btn-success m-3">
+            <Link
+              to="/dashboard/add-appointment"
+              className="btn btn-success m-3"
+            >
               Book an appointment
             </Link>
           </div>
@@ -140,10 +157,47 @@ const Home = () => {
         <div className="px-5">
           <table className="table">
             <thead>
+              <tr>
+                <th>Date</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Service</th>
+                <th>Pet</th>
+                <th>Veterinarian</th>
+                <th>Cost</th>
+              </tr>
             </thead>
             <tbody>
+              {events.length > 0 ? (
+                events.map((event) => (
+                  <tr key={event.appointments_id}>
+                    <td>
+                      {moment(event.appoitments_starts_at).format("DD-MM-YYYY")}
+                    </td>
+                    <td>{moment(event.appoitments_starts_at).format("LT")}</td>
+                    <td>{moment(event.appointments_ends_at).format("LT")}</td>
+                    <td>{event.service_name}</td>
+                    <td>{event.pet_name}</td>
+                    <td>{event.veterinarian_name}</td>
+                    <td>{event.service_price} €</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="6">
+                    <FaCalendarAlt /> No appointments found for the selected
+                    date range.
+                  </td>
+                </tr>
+              )}
               <tr>
-                <td> <FaCalendarAlt /> No appointments selected. Please select a date.</td>
+                <td>Total Cost:</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>{totalCost} €</td>
               </tr>
             </tbody>
           </table>
@@ -157,47 +211,57 @@ const Home = () => {
           </Link>
         </div>
         <div className="mt-3">
-        {admins.length === 0 ? (
-          <table className="table">
-          <thead>
-            <tr>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-              <tr>
-                <td>
-                  <RiAdminFill /> No Admin Found
-                </td>
-              </tr>
-          </tbody>
-        </table>
-        ) : (
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Email</th>
-                <th>Availability</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {admins.map((admin) => (
-                <tr key={admin.id}>
-                  <td>{admin.email}</td>
+          {admins.length === 0 ? (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
                   <td>
-                    <FaCircle color="green" /> Available
-                  </td>
-                  <td>
-                    <Link to={`/dashboard/edit-admin/` + admin.id} className="btn btn-success btn-sm me-2">Edit</Link>
-                    <button className="btn btn-danger btn-sm" onClick={() => handleDeleteAdmin(admin.id)}>Delete</button>
+                    <RiAdminFill /> No Admin Found
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
+              </tbody>
+            </table>
+          ) : (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Email</th>
+                  <th>Availability</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {admins.map((admin) => (
+                  <tr key={admin.id}>
+                    <td>{admin.email}</td>
+                    <td>
+                      <FaCircle color="green" /> Available
+                    </td>
+                    <td>
+                      <Link
+                        to={`/dashboard/edit-admin/` + admin.id}
+                        className="btn btn-success btn-sm me-2"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        className="btn btn-danger btn-sm"
+                        onClick={() => handleDeleteAdmin(admin.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
       </div>
     </div>
   );
